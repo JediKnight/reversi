1c2640f8fca6c5f633fb1d2c4d4aac4731c568f9
il_tanaka <tanaka@infiniteloop.co.jp>
2012-07-11 21:28:44 +0900

    search.c .h 消えて泣きそうだったから新しく作り直して追加した

---
+++ search.c
@@ -0,0 +1,128 @@
+#include "search.h"
+
+int flip(int *b, int s, int x, int y)
+{
+  int xx, yy, xxx, yyy, flg = 0;
+
+  /* x から右にサーチ */
+  if(b[getpos((x + 1), y)] == reverse(s))
+    {
+      for(xx = x + 1; xx < BOARD_WIDTH; xx++)
+	{
+	  if(b[getpos(xx, y)] == s)
+	    {
+	      for(xxx = x + 1; xxx < xx; xxx++)
+		b[getpos(xxx, y)] = s;
+	      flg++;
+	      break;
+	    }
+	}
+    }
+
+  /* x から左にサーチ */
+  if(b[getpos((x - 1), y)] == reverse(s))
+    {
+      for(xx = x - 1; xx >= 0; xx--)
+	{
+	  if(b[getpos(xx, y)] == s)
+	    {
+	      for(xxx = x - 1; xxx >= xx; xxx--)
+		b[getpos(xxx, y)] = s;
+	      flg++;
+	      break;
+	    }
+	}
+    }
+
+  /* y から下にサーチ */
+  if(b[getpos(x, (y + 1))] == reverse(s))
+    {
+      for(yy = y + 1; yy < BOARD_HEIGHT; yy++)
+	{
+	  if(b[getpos(x, yy)] == s)
+	    {
+	      for(yyy = y + 1; yyy < yy; yyy++)
+		b[getpos(x, yyy)] = s;
+	      flg++;
+	      break;
+	    }
+	}
+    }
+
+  /* y から上にサーチ */
+  if(b[getpos(x, (y - 1))] == reverse(s))
+    {
+      for(yy = y - 1; yy >= 0; yy--)
+	{
+	  if(b[getpos(x, yy)] == s)
+	    {
+	      for(yyy = y - 1; yyy >= yy; yyy--)
+		b[getpos(x, yyy)] = s;
+	      flg++;
+	      break;
+	    }
+	}
+    }
+
+  /* xy から右下にサーチ */
+  if(b[getpos((x + 1), (y + 1))] == reverse(s))
+    {
+      for(xx = x + 1, yy = y + 1; xx < BOARD_WIDTH && yy < BOARD_HEIGHT; xx++, yy++)
+	{
+	  if(b[getpos(xx, yy)] == s)
+	    {
+	      for(xxx = x + 1, yyy = y + 1; xxx < xx && yyy < yy; xxx++, yyy++)
+		b[getpos(xxx, yyy)] = s;
+	      flg++;
+	      break;
+	    }
+	}
+    }
+
+  /* xy から左下にサーチ */
+  if(b[getpos((x - 1), (y + 1))] == reverse(s))
+    {
+      for(xx = x - 1, yy = y + 1; xx >= 0 && yy < BOARD_HEIGHT; xx--, yy++)
+	{
+	  if(b[getpos(xx, yy)] == s)
+	    {
+	      for(xxx = x - 1, yyy = y + 1; xxx >= xx && yyy < yy; xxx--, yyy++)
+		b[getpos(xxx, yyy)] = s;
+	      flg++;
+	      break;
+	    }
+	}
+    }
+
+  /* xy から右上にサーチ */
+  if(b[getpos((x + 1), (y - 1))] == reverse(s))
+    {
+      for(xx = x + 1, yy = y - 1; xx < BOARD_WIDTH && yy >= 0; xx++, yy--)
+	{
+	  if(b[getpos(xx, yy)] == s)
+	    {
+	      for(xxx = x + 1, yyy = y - 1; xxx < xx && yyy >= yy; xxx++, yyy--)
+		b[getpos(xxx, yyy)] = s;
+	      flg++;
+	      break;
+	    }
+	}
+    }
+
+  /* xy から左上にサーチ */
+  if(b[getpos((x - 1), (y - 1))] == reverse(s))
+    {
+      for(xx = x - 1, yy = y - 1; xx >= 0 && yy >= 0; xx--, yy--)
+	{
+	  if(b[getpos(xx, yy)] == s)
+	    {
+	      for(xxx = x - 1, yyy = y - 1; xxx >= xx && yyy >= yy; xxx--, yyy--)
+		b[getpos(xxx, yyy)] = s;
+	      flg++;
+	      break;
+	    }
+	}
+    }
+
+  return flg;
+}
---
+++ search.h
@@ -0,0 +1,8 @@
+#ifndef _SEARCH_H_
+#define _SEARCH_H_
+
+#include "hage.h"
+
+int flip(int *b, int s, int x, int y);
+
+#endif	/* _SEARCH_H_ */

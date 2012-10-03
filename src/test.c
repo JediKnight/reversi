82bc8fcc760abe6cfe0f7fa5b7ae617dc5b70d60
たなかともあき <github@bamboo-copter.com>
2012-10-03 19:59:31 +0900

    ;aljf;lasjfd;oajfopagoqbwfepwqpef

--- src/test.c
+++ src/test.c
@@ -2,47 +2,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int numberOfDigit(int val)
+int nofd(int val)
 {
   int ret = 0;
   for(; val > 0; val /= 10, ret++);
   return ret;			/* return sprintf(dummy, "%d", val); */
 }
 
-int separate(int val, int digit, int targ)
+int numsep(int val, int digit, int targ)
 {
   char v[digit];
   sprintf(v, "%d", val);
   return v[digit - targ] - '0';
 }
 
-int btod(int decimal, int val)
+int btod(int dec, int val)
 {
-  int i, ret = 0, digit = numberOfDigit(val);
-  for(i = digit - 1; i >= 0; ret += (int)pow((double)decimal, (double)i) * separate(val, digit, i + 1), i--);
+  int i, ret = 0, digit = nofd(val);
+  for(i = digit - 1; i >= 0; ret += (int)pow((double)dec, (double)i) * numsep(val, digit, i + 1), i--);
   return ret;
 }
 
-int dtob(int decimal, int val)
+int dtob(int dec, int val)
 {
-  int i, ret = 0, digit = numberOfDigit(val);
-  for(i = digit - 1; i >=0; ret += (val % decimal) * (int)pow((double)10, (double)1));
+  int quo = val, rem = val, ret = 0;
+
+  for(;(quo /= dec) > 0;)
+    {
+      ret *= 10;
+      rem %= dec;
+      printf("%d %d\n", quo, rem);
+      ret += rem;
+      rem = quo;
+    }
   return ret;
 }
 
 int main(int argc, char **argv)
 {
-  int org, dec;
+  int orig, decimal;
 
   if(argc < 3) return -1;
 
-  dec = atoi(argv[1]);
-  org = atoi(argv[2]);
+  decimal = atoi(argv[1]);
+  orig = atoi(argv[2]);
 
-  if(dec == 10)
-    printf("%d\n", btod(dec, org));
+  if(decimal == 10)
+    printf("%d\n", btod(decimal, orig));
   else
-    printf("%d\n", btod(2, org));
+    printf("%016d\n", dtob(decimal, orig));
 
   return 0;
 }
